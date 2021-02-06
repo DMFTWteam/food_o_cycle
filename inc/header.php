a6834094ae1e71b1f45b25068fe2dff0c1eec41c
gid3877 <78322953+gid3877@users.noreply.github.com>
2021-02-06 10:28:12 -0500

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Site Metas -->
-    <title>Food O' Cycle</title>
+    <title>Food O' Cycle - CIS 492</title>
     <meta name="keywords" content="">
     <meta name="description" content="">
     <meta name="author" content="">
