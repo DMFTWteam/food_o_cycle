72fce33dc9b4b3a628c51370f8356ee0fe6639f8
gid3877 <gid3877@calu.edu>
2021-02-15 08:41:33 -0500

    Update
    Added admin.php and resetPassword.php

---
+++ admin.php
@@ -0,0 +1,85 @@
+<?php
+	include 'inc/header.php';
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+	<body>
+		<div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="table-main table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Food Banks</th>
+									<th>Food Donors</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a href="#">Sample Food Bank #1</a></td>
+									<td><a href="#">Sample Food Donor #1</a></td>
+                                </tr>
+								<tr>
+                                    <td><a href="#">Sample Food Bank #2</a></td>
+									<td><a href="#">Sample Food Donor #2</a></td>
+                                </tr>
+								<tr>
+                                    <td><a href="#">Sample Food Bank #3</a></td>
+									<td><a href="#">Sample Food Donor #3</a></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+			<div class="row">
+                <div class="col-lg-12">
+                    <div class="table-main table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Business Name</th>
+									<th>Username</th>
+									<th>Date/Time Accessed</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Sample</td>
+									<td>Sample</td>
+									<td>Sample</td>
+                                </tr>
+								<tr>
+                                    <td>Sample</td>
+									<td>Sample</td>
+									<td>Sample</td>
+                                </tr>
+								<tr>
+                                    <td>Sample</td>
+									<td>Sample</td>
+									<td>Sample</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+			<div class="row">
+                <div class="col-lg-12">
+					<button type="submit" class="btn hvr-hover">Download Logs</button>
+				</div>
+			</div>
+        </div>
+				
+	</body>
+</html>
+
+<?php
+	include 'inc/js_to_include.php';
+	include 'inc/footer.php';
+	
+?>
---
+++ resetPassword.php
@@ -0,0 +1,35 @@
+<?php
+	include 'inc/header.php';
+	
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- Basic -->
+	<body>
+		<div class="Center col-sm-6 col-lg-6 mb-3" >
+                    <div class="title-center">
+                        <h3>Reset Password</h3>
+                    </div>
+                    <form class="mt-3 review-form-box" id="formLogin" style="margin-bottom: 30%;">
+                        <div class="form-row" >
+                            <div class="form-group col-md-6">
+                                <label for="InputPassword" class="mb-0">New Password</label>
+                                <input type="password" class="form-control" id="InputPassword" placeholder="Enter Password"> </div>
+                            <div class="form-group col-md-6">
+                                <label for="ConfirmPassword" class="mb-0">Confirm Password</label>
+                                <input type="password" class="form-control" id="ConfirmPassword" placeholder="Confirm Password"> </div>
+                        </div>
+                        <button type="submit" class="btn hvr-hover">Login</button>
+						<button type="submit" class="btn hvr-hover">Cancel</button>
+                    </form>
+                </div>
+				
+	</body>
+</html>
+
+<?php
+	include 'inc/js_to_include.php';
+	include 'inc/footer.php';
+	
+?>
