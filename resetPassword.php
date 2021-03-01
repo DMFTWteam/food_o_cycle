77498d62478a0ac8d0d372f52c95d9373772f8f9
gid3877 <gid3877@calu.edu>
2021-03-01 14:40:52 -0500

    update

--- resetPassword.php
+++ resetPassword.php
@@ -25,14 +25,15 @@
 		<div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <form class="mt-3 review-form-box" id="formLogin" style="margin-bottom: 30%;">
+                    <form class="mt-3 review-form-box" name="formLogin" style="margin-bottom: 30%;" action='login.php' method='post'>
                         <div class="form-row" >
                             <div class="form-group col-md-6">
                                 <label for="InputPassword" class="mb-0">New Password</label>
-                                <input type="password" class="form-control" id="InputPassword" placeholder="Enter Password"> </div>
+                                <input type="password" class="form-control" name="InputPassword" placeholder="Enter Password"> </div>
                             <div class="form-group col-md-6">
                                 <label for="ConfirmPassword" class="mb-0">Confirm Password</label>
-                                <input type="password" class="form-control" id="ConfirmPassword" placeholder="Confirm Password"> </div>
+                                <input type="password" class="form-control" name="ConfirmPassword" placeholder="Confirm Password">
+								<input type='hidden' name='path' value='<?php echo $_SERVER['PHP_SELF']; ?>'/> </div>
                         </div>
                         <button type="submit" class="btn hvr-hover">Submit</button>
 						<button type="submit" class="btn hvr-hover">Cancel</button>
