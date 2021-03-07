e17abdf92922d662ed3e991c14ac62e88a0f27cf
gid3877 <gid3877@calu.edu>
2021-03-07 16:41:44 -0500

    alex

--- about.php
+++ about.php
@@ -19,11 +19,36 @@ include 'inc/header.php';
             <!-- End All Title Box -->
 			<div class="row">
 				<div class="col-lg-4 offset-lg-4">
-					<div class="about-content">
+					<div class="about-content" style='margin-bottom: 20px'>
 						Food O' Cycle started off with the simple idea of reducing waste,
-						while trying to reduce community hunger at the same time. To do so, we had to come up with
-						a way of connecting businesses that could donate, and the organizations that could accept these 
-						donations.
+						while trying to reduce community hunger at the same time. 
+
+                        Our mission is to help others who are in need by not letting food go to waste. We want to bring 
+                        people together and work as a community to support an important cause.
+
+                        We offer the ability for companies that have extra food that they will not be utilizing, to donate 
+                        it instead of throwing it out. This has been designed so that food banks can easily and efficiently 
+                        find what they are in need of as well as make it quick and efficient for companies to donate the food. 
+
+                        One of the founders of this company, Fred McGallahan, was a waiter at one of his local restaurants in an 
+                        area of poverty. He saw first hand the amount of food that was being wasted on a daily basis as well 
+                        as the amount of people in the area in desperate need of food. Wishing he could do more to help the 
+                        community he grew up in, he came up with the idea to take food that was going to be thrown out by the 
+                        restaurant to local shelters and food banks. Seeing how much that helped the shelters and food banks, 
+                        he assumed that most other restaurants were doing the same with unused food and went around to gather 
+                        the food from local restaurants. Every week, he was making deliveries to shelters and food banks. The word 
+                        was spreading quickly about what Fred was doing and other people in the community wanted to help. 
+                        Other cities heard about what was happening and started doing the same thing. Once Fred realized how 
+                        much, not only his community, but communities all of the country needed this service, he helped establish 
+                        a team together that would be able to create the service online to make it more convenient for everyone 
+                        involved. 
+                        
+                        To do so, our team had to come up with a way of connecting businesses that could donate, and 
+                        the organizations that could accept these donations and this is the result!
+
+                        We hope you enjoy!
+
+
 					</div>
 				</div>
 			</div>
