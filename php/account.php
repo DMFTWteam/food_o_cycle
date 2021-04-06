031acaeba7e629301c1c73eb6193114b1601d75d
gid3877 <gid3877@calu.edu>
2021-04-06 11:01:36 -0400

    alex

--- php/account.php
+++ php/account.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['user'])) {
         } else {
             //header("Location: ../fbhome.php");
             echo "bank";
-            print_r($_SESSION['business']['business_is_donor']);
+            var_dump($_SESSION['business']['business_is_donor']);
             exit();
         }
         
