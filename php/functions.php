452745e4cb7a0c1ee1bad0fe466e59c94deaa6fc
gid3877 <gid3877@calu.edu>
2021-04-06 15:20:04 -0400

    alex

--- php/functions.php
+++ php/functions.php
@@ -11,54 +11,54 @@
  * @license  https://www.gnu.org/licenses/gpl-3.0.en.html GNU Public License v3.0
  * @link     https://github.com/DMFTWteam/food_o_cycle
  */
+
 function tableBusinessNames()
 {
-    echo 'function executed';
     include "../inc/db_connect.php";
     $query = 'SELECT business_id, business_name, business_is_donor 
-            FROM business
+            FROM businesses
 		    ORDER BY business_name';
     $statement = $db->prepare($query);
     $statement->execute();
     $businesses = $statement->fetchAll();
     $statement->closeCursor();
-    
-    print_r($businesses);
+
     $donors = array();
     $banks = array();
+    $tableString = '';
 
     foreach ($businesses as $item) {
-        if ($item['business_is_donor'] === 1) {
+        if ($item['business_is_donor'] == 1) {
             array_push($donors, $item);
         } else {
             array_push($banks, $item);
         }
     }
-    print_r($donors);
-    print_r($banks);
+
     $min_num = min(count($donors), count($banks));
     $i = 0;
     while ($i < $min_num) {
-        echo '<tr>
-                <td> $banks[$i][\'business_name\'] </td>
-                <td> $donors[$i][\'business_name\'] </td>
+        $tableString += '<tr>
+                <td>'.$banks[$i]['business_name'].'</td>
+                <td>'.$donors[$i]['business_name'].'</td>
                 </tr>';
         $i++;
     }
     
     foreach (max($donors, $banks) as $item) {
         if ($item['business_is_donor'] == 1) {
-            echo '<tr>
+            $tableString += '<tr>
                 <td></td>
-                <td> $item[\'business_name\'] </td>
+                <td>'.$item['business_name'].'</td>
                 </tr>';
         } else {
-            echo '<tr>
-                <td> $item[\'business_name\'] </td>
+            $tableString += '<tr>
+                <td>'.$item['business_name'].'</td>
                 <td></td>
                 </tr>';
         }
     }
+    return $tableString;
 }
 
 /*function tableAccessLogs($business)
