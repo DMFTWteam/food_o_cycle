06e4d16d4bec0d397196b95c183f0414fbea2f2d
gid3877 <gid3877@calu.edu>
2021-04-12 09:29:33 -0400

    alex

--- index.php
+++ index.php
@@ -216,7 +216,7 @@ $_SESSION['cart'] = array();
                     echo "<div class='product-id display-none'>{$item['item_id']}</div>";
          
                     // enable add to cart button
-                    echo "<button style='width:100%;' type='submit' class='btn btn-primary cart'>";
+                    echo "<button style='width:100%;' type='submit' class='cart'>";
                         echo "Add to cart";
                     echo "</button>";
          
