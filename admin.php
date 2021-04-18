f552d53851b8e5e6181b03dc558497d0a948a011
gid3877 <gid3877@calu.edu>
2021-04-18 16:46:28 -0400

    alex

--- admin.php
+++ admin.php
@@ -172,7 +172,7 @@ try {
     <script>
     $(document).ready(function(){
         $("#download").on("click", function(){
-            var doc = new jsPDF()
+            var doc = new jspdf()
             doc.autoTable({ html: '#log_table', styles: {halign: 'center', theme: 'grid'}})
             doc.save('access_logs.pdf')
         });
