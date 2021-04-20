35c8a302994553506fb3c5b5d4668edd8d74fe61
gid3877 <gid3877@calu.edu>
2021-04-19 21:04:05 -0400

    alex

--- php/user_actions.php
+++ php/user_actions.php
@@ -6,9 +6,12 @@ try {
     $value = filter_input(INPUT_GET, 'est_val');
     $value = filter_input(INPUT_GET, 'perish');
     $value = filter_input(INPUT_GET, 'expDate');
+
     include "../inc/header.php";
     include "../donorhome.php";
-    include "../footer.php";
+    include "../inc/js_to_include.php";
+    include "../inc/footer.php";
+
 } catch(Exception $e) {
     header("Location: inc/error.php?msg=" .urlencode($e->getMessage()));
 }
