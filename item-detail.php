41a12a004cce8b1ca3cd165874f7499d4a6499bd
gid3877 <gid3877@calu.edu>
2021-04-23 21:40:12 -0400

    alex

--- item-detail.php
+++ item-detail.php
@@ -19,7 +19,7 @@ $statement->execute();
 $item_info = $statement->fetch();
 $statement->closeCursor();
 
-
+print_r($_SESSION['cart']);
 
 ?>
 
@@ -30,8 +30,8 @@ $statement->closeCursor();
             <div class="col-lg-12">
                 <h2>Shop Detail</h2>
                 <ul class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Shop</a></li>
-                    <li class="breadcrumb-item active">Shop Detail </li>
+                    <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                    <li class="breadcrumb-item active">Item Detail </li>
                 </ul>
             </div>
         </div>
@@ -44,44 +44,56 @@ $statement->closeCursor();
     <div class="container">
         <div class="row">
             <div class="col-xl-5 col-lg-5 col-md-6">
-                
-            </div>
-            <div class="col-xl-7 col-lg-7 col-md-6">
-                <div class="single-product-details">
-                    <h2>Fachion Lorem ipsum dolor sit amet</h2>
-                    <h5> <del>$ 60.00</del> $40.79</h5>
-                    <p class="available-stock"><span> More than 20 available / <a href="#">8 sold </a></span>
-                    <p>
-                    <h4>Short Description:</h4>
-                    <p>Nam sagittis a augue eget scelerisque. Nullam lacinia consectetur sagittis. Nam sed neque id eros
-                        fermentum dignissim quis at tortor. Nullam ultricies urna quis sem sagittis pharetra. Nam erat
-                        turpis, cursus in ipsum at,
-                        tempor imperdiet metus. In interdum id nulla tristique accumsan. Ut semper in quam nec pretium.
-                        Donec egestas finibus suscipit. Curabitur tincidunt convallis arcu. </p>
-                    <ul>
-                        <li>
-                            <div class="form-group quantity-box">
-                                <label class="control-label">Quantity</label>
-                                <input class="form-control" value="0" min="0" max="20" type="number">
-                            </div>
-                        </li>
-                    </ul>
+                <div id="carousel-example-1" class="single-product-slider carousel slide">
+                    <div class="carousel-inner" role="listbox">
+                        <?php 
+                        if ($item_info['item_image'] == null || $item_info['item_image'] == '') {
+                            echo "<img class=\"d-block w-100 img-fluid\" src='https://via.placeholder.com/300.jpg?text=No+Image+Found' />";
+                        } else {
+                            echo "<img class=\"d-block w-100 img-fluid\" src='data:image/jpeg;charset=utf8;base64," . base64_encode($item_info['item_image']) . "' />";
+                        } ?>
+                    </div>
 
-                    <div class="price-box-bar">
-                        <div class="cart-and-bay-btn">
-                            <a class="btn hvr-hover" data-fancybox-close="" href="#">Add to cart</a>
+
+                </div>
+                <div class="col-xl-7 col-lg-7 col-md-6">
+                    <div class="single-product-details">
+                        <h2><?php echo $item_info['item_desc']; ?></h2>
+                        <h5><?php echo $item_info['business_name']; ?></h5>
+                        <p class="available-stock"><span><?php echo $item_info['item_qty_avail']; ?> available</span>
+                        <p>
+                            <?php 
+                            if ($item_info['item_perishable'] == 1) {
+                                $date = date("m-d-Y", strtotime($item_info['item_expiration']));
+                                echo "<h4>Expiration: {$date}</h4>";
+                            } else {
+                                echo "<h4>Non-perishable Item</h4>";
+                            } ?>
+                        <p></p>
+                        <ul>
+                            <li>
+                                <div class="form-group quantity-box">
+                                    <label class="control-label">Quantity</label>
+                                    <input class="form-control" value="0" min="0" max="<?php echo $item_info['item_qty_avail']; ?>" type="number">
+                                </div>
+                            </li>
+                        </ul>
+
+                        <div class="price-box-bar">
+                            <div class="cart-and-bay-btn">
+                                <a class="btn hvr-hover" data-fancybox-close="" href="#">Add to cart</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+
+
         </div>
-
-        
-
     </div>
-</div>
-<!-- End Cart -->
+    <!-- End Cart -->
 
-<?php 
+    <?php 
     require "inc/footer.php";
-?>
+    ?>
