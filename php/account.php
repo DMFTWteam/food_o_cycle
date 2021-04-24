9e0d5538319129ccbede4d5f8b95308f23f56553
gid3877 <gid3877@calu.edu>
2021-04-23 20:46:51 -0400

    alex

--- php/account.php
+++ php/account.php
@@ -60,46 +60,46 @@ try {
                 if ($validPassword) {
                     $_SESSION['user'] = $user_info;
                     
-                    /* if (isset($_SESSION['path']) && $_SESSION['path'] != '' && $_SESSION['path'] != '/account.php') {
+                    if (isset($_SESSION['path']) && $_SESSION['path'] != '' && $_SESSION['path'] != '/account.php') {
                         header("Location: .." .$_SESSION['path']);
                         exit();
-                    } else { */
-                    if ($user_info['u_is_admin'] == 1) {
-                        Log_access($user_info['u_id'], 1);
-                        header("Location: ../admin.php");
-                        exit();
-                    } else if ($user_info['u_is_standard'] == 1) {
+                    } else {
+                        if ($user_info['u_is_admin'] == 1) {
+                            Log_access($user_info['u_id'], 1);
+                            header("Location: ../admin.php");
+                            exit();
+                        } else if ($user_info['u_is_standard'] == 1) {
                         
-                        //Log_access($user_info['u_id'], '1');
+                            Log_access($user_info['u_id'], '1');
                         
-                        $query2 = 'SELECT *
+                            $query2 = 'SELECT *
                                       FROM user_to_business, business
                                      WHERE user_to_business.u_id = :u_id
                                     AND user_to_business.business_id = business.business_id';
-                        $statement2 = $db->prepare($query2);
-                        $statement2->bindValue(':u_id', $user_info['u_id']);
-                        $statement2->execute();
-                        $bus_info = $statement2->fetch();
-                        $business_count = $statement2->rowCount();
-                        $statement2->closeCursor();
-                        if ($business_count > 0) {
-                             $_SESSION['business'] = $bus_info;
-                            if ($_SESSION['business']['business_is_donor'] == 1) {
-                                header("Location: ../donorhome.php");
-                                exit();
-                            } else {
-                                header("Location: ../fbhome.php");
-                                exit();
+                            $statement2 = $db->prepare($query2);
+                            $statement2->bindValue(':u_id', $user_info['u_id']);
+                            $statement2->execute();
+                            $bus_info = $statement2->fetch();
+                            $business_count = $statement2->rowCount();
+                            $statement2->closeCursor();
+                            if ($business_count > 0) {
+                                 $_SESSION['business'] = $bus_info;
+                                if ($_SESSION['business']['business_is_donor'] == 1) {
+                                    header("Location: ../donorhome.php");
+                                    exit();
+                                } else {
+                                    header("Location: ../fbhome.php");
+                                    exit();
+                                }
                             }
                         }
                     }
-                    //}
                 } else {
-                    //Log_access($user_info['u_id'], '0');
+                    Log_access($user_info['u_id'], '0');
                     echo '<label>Invalid Password.</label>'; 
                 }
             } else {
-                //Log_access($user_info['u_id'], '0');
+                Log_access($user_info['u_id'], '0');
                 echo '<label>Invalid Username.</label>'; 
             }
         }
