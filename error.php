260b2ec75fb8b6d5e513cc8e66617c084bb75c8b
gid3877 <gid3877@calu.edu>
2021-04-23 21:03:35 -0400

    alex

--- error.php
+++ error.php
@@ -1,5 +1,5 @@
 <?php
-    require '../inc/header.php';
+    require 'inc/header.php';
     $msg = filter_input(INPUT_GET, 'msg');
     $error = "<h1 style='color: red;'>".$msg."</h1>";
     echo $error; 
@@ -9,7 +9,7 @@
 function countdown() {
     var i = document.getElementById('counter');
     if (parseInt(i.innerHTML) <= 0) {
-        location.href = '../index.php';
+        location.href = 'index.php';
     }
     if (parseInt(i.innerHTML) != 0) {
         i.innerHTML = parseInt(i.innerHTML) - 1;
@@ -21,7 +21,7 @@ setInterval(function() {
 </script>
 
 <?php
-    require '../inc/js_to_include.php';
-    require '../inc/footer.php';
+    require 'inc/js_to_include.php';
+    require 'inc/footer.php';
     
 ?>
