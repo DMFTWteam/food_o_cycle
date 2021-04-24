bf7c40f34cc914754e84eadd87274c71b5e3fd8d
gid3877 <gid3877@calu.edu>
2021-04-23 21:00:00 -0400

    alex

--- php/error.php
+++ php/error.php
@@ -1,22 +1,24 @@
 <?php
     require '../inc/header.php';
     $msg = filter_input(INPUT_GET, 'msg');
-    $error = "<h1 style='color: red; '>".$msg."</h1>";
+    $error = "<h1 style='color: red;'>".$msg."</h1>";
     echo $error; 
 ?>
 <p>You will be redirected to home page in <span id='counter'>5</span> second(s).</p>
-    <script type='text/javascript'>
-    function countdown() {
-        var i = document.getElementById('counter');
-        if (parseInt(i.innerHTML)<=0) {
-            location.href = '../index.php';
-        }
-        if (parseInt(i.innerHTML)!=0) {
-        i.innerHTML = parseInt(i.innerHTML)-1;
-        }
+<script type='text/javascript'>
+function countdown() {
+    var i = document.getElementById('counter');
+    if (parseInt(i.innerHTML) <= 0) {
+        location.href = '../index.php';
     }
-    setInterval(function(){ countdown(); },1000);
-    </script>
+    if (parseInt(i.innerHTML) != 0) {
+        i.innerHTML = parseInt(i.innerHTML) - 1;
+    }
+}
+setInterval(function() {
+    countdown();
+}, 1000);
+</script>
 
 <?php
     require '../inc/js_to_include.php';
